df5b460429273d9bc2576a034c988e2e4da96770
Kooreis <kooreis.official@gmail.com>
2022-12-13 14:03:49 -0600

    Added two nested for loops to iterate over the matrix. The outer loop iterates over each layer of the matrix, starting from the outermost layer and moving inwards. The inner loop iterates over each element in the current layer.

--- solution.ts
+++ solution.ts
@@ -1,5 +1,9 @@
 class MatrixRotator {
     rotate(matrix: number[][]): void {
         const n = matrix.length;
+        for (let i = 0; i < n / 2; i++) {
+            for (let j = i; j < n - i - 1; j++) {
+            }
+        }
     }
 }
